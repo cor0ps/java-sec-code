b51f9b2786bcdce7a66dfbc224dcaa4ab6fbbe19
Buglab <taochao@huawei.com>
2019-03-25 15:29:49 +0800

    Update XXE.java

--- src/main/java/com/org/buglab/seccoding/controller/XXE.java
+++ src/main/java/com/org/buglab/seccoding/controller/XXE.java
@@ -5,10 +5,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class XXE {
-    @RequestMapping("/xxe")
-    public String xxe(){
-        String xxx="ceshi";
-        return xxx;
+   @RequestMapping(value = "/SAXReader", method = RequestMethod.POST)
+    public String SAXReader(HttpServletRequest request) {
+        try {
+            InputStream in = request.getInputStream();
+            SAXReader saxReader = new SAXReader();
+            //生成document文件
+            Document document = saxReader.read(in);
+            // 得到xml根元素       
+            Element root = document.getRootElement();
+            // 得到根元素的所有子节点
+            List<Element> elementList = root.elements();
+            // 遍历所有子节点
+            for (Element e : elementList) {
+                //map.put(e.getName(), e.getText());
+                System.out.println(e.getName() + ":" + e.getText());
+            }
+            // 释放资源       
+            in.close();
+            in = null;
+        } catch (IOException | DocumentException e) {
+            e.printStackTrace();
+        }
+        return "OK";
     }
 
 

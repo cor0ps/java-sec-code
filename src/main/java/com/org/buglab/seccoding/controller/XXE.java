0510af673e6fc47e2865dca58d7d329ed663fc9c
Buglab <taochao@huawei.com>
2019-03-27 16:31:15 +0800

    parse xml

--- src/main/java/com/org/buglab/seccoding/controller/XXE.java
+++ src/main/java/com/org/buglab/seccoding/controller/XXE.java
@@ -98,7 +98,46 @@ public class XXE {
         }
         return "Not Fix";
     }
-
+   
+ private void praseElement(org.w3c.dom.Element element) {
+        String tagName = element.getNodeName();
+        NodeList children = element.getChildNodes();
+        System.out.print("<" + tagName);
+        //element 元素的所有属性所构成的 NamedNodeMap 对象，需要对其进行判断
+        NamedNodeMap map = element.getAttributes();
+        //如果该元素存在属性
+        if (null != map) {
+            for (int i = 0; i < map.getLength(); i++) {
+                //获得该元素的每一个属性
+                Attr attr = (Attr) map.item(i);
+                String attrName = attr.getName();
+                String attrValue = attr.getValue();
+                System.out.print(" " + attrName + "=\"" + attrValue + "\"");
+            }
+        }
+        System.out.print(">");
+        for (int i = 0; i < children.getLength(); i++) {
+            Node node = children.item(i);
+            //获得结点的类型
+            short nodeType = node.getNodeType();
+            if (nodeType == Node.ELEMENT_NODE) {
+                //是元素，继续递归
+                praseElement((org.w3c.dom.Element) node);
+            } else if (nodeType == Node.TEXT_NODE) {
+                //递归出口
+                System.out.print(node.getNodeValue());
+            } else if (nodeType == Node.COMMENT_NODE) {
+                System.out.print("<!--");
+                Comment comment = (Comment) node;
+                //注释内容
+                String data = comment.getData();
+                System.out.print(data);
+                System.out.print("-->");
+            }
+        }
+        System.out.print("</" + tagName + ">");
+    }
+   
     @RequestMapping(value = "/DocumentBuilderFactory-fix", method = RequestMethod.POST)
     public String Fix_DocumentBuilderFactory(HttpServletRequest request)
     {

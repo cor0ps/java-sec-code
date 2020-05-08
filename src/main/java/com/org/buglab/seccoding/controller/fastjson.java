19c1c26bec08cef93e7c1d0532d068ce10fe2d38
Buglab <taochao@huawei.com>
2020-05-08 09:28:14 +0800

    modify code

--- src/main/java/com/org/buglab/seccoding/controller/fastjson.java
+++ src/main/java/com/org/buglab/seccoding/controller/fastjson.java
@@ -1,14 +1,23 @@
 package com.org.buglab.seccoding.controller;
 
-@Controller
-public class fastjson {
 
-    @RequestMapping(value = "/fastjson",method = RequestMethod.POST)
-    public  JSONObject fastjson(@RequestBody String data){
-        JSONObject object=JSONObject.parseObject(data,Feature.SupportNonPublicField);
+@RestController
+@RequestMapping("/fastjson")
+public class fastjson {
+    @RequestMapping(value = "/1.2.24",method = RequestMethod.GET)
+    public String fastjson()
+    {
+        return "fastjson 1.2.24";
+    }
+
+
+    @RequestMapping(value = "/1.2.24",method = RequestMethod.POST)
+    public JSONObject fastjson(@RequestBody String data){
+        //JSONObject object=JSONObject.parseObject(data, Student.class,Feature.SupportNonPublicField);
+        Student student=JSONObject.parseObject(data, Student.class,Feature.SupportNonPublicField);
         JSONObject ret=new JSONObject();
         ret.put("code",1001);
-        ret.put("data","Hello "+object.get("name"));
+        ret.put("data","Hello "+student.getName());
         return ret;
 
     }
